342a78cc130d58b03022f0170906b0b134ec3510
BlackHat(س.مرتضی) <www.mortezamosavy0910@gmail.com>
2016-02-15 17:15:12 +0330

    Create Version.lua

---
+++ plugins/Version.lua
@@ -0,0 +1,18 @@
+do
+
+function run(msg, matches)
+  return 'Bot '.. VERSION .. [[ 
+ورژن ربات: 2
+ایدی سودوها:
+@Black_hat_admin02
+@Black_hat_admin03
+@mamootcode
+]]
+end
+
+return {
+  description = "Shows bot version", 
+  usage = "ورژن: Shows bot version",
+  patterns = {
+    "^ورژن$"
+  }, 
